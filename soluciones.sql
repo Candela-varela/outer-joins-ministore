c6ab293d36f1d124453c1dd86859f15f735d2bd8
Candela-varela <candelavarela1@gmail.com>
2026-09-24 12:37:54 -0300

    Add files via upload

--- soluciones.sql
+++ soluciones.sql
@@ -10,15 +10,15 @@
 -- Los productos sin ventas aparecerán con NULL en las columnas de ventas.
 
 SELECT P.nombre, V.venta_id
-FROM Productos P LEFT JOIN Ventas V ON P.producto_id = V. producto_id WHERE V.venta_id IS NULL
+FROM Productos P LEFT JOIN Ventas V ON P.producto_id = V. producto_id
 
 -- ── CONSULTA 2: RIGHT JOIN ────────────────
 -- Pregunta de negocio: ¿Existen ventas registradas con productos
 -- que no figuran en nuestro catálogo? (posible error de carga de datos)
 -- Los registros huérfanos aparecerán con NULL en las columnas de productos.
 
-SELECT V.venta_id, P.nombre
-FROM Productos P RIGHT JOIN Ventas V ON P.producto_id = V. producto_id WHERE P.nombre IS NULL
+SELECT V.venta_id, P.producto_id
+FROM Productos P RIGHT JOIN Ventas V ON P.producto_id = V. producto_id WHERE P.producto_id IS NULL
 
 -- ── CONSULTA 3: FULL OUTER JOIN ───────────
 -- Pregunta de negocio: Vista completa de auditoría que muestre
